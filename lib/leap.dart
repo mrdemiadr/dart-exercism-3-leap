2cc278f038be1aa113e5b60137bf7fff88275ea7
mrdemiadr <demiadidrana@gmail.com>
2020-07-10 22:45:40 +0700

    revisi menggunakan 1 line if

--- lib/leap.dart
+++ lib/leap.dart
@@ -1,18 +1,7 @@
 class Leap {
   bool leapYear(int year) {
-    bool isLeapYears;
-    if (isDivideByFour(year) == true && isDivideByOneHundred(year) == false) {
-      isLeapYears = true;
-    } else if (isDivideByFourHundred(year) == true) {
-      isLeapYears = true;
-    } else {
-      isLeapYears = false;
-    }
-
+    bool isLeapYears =
+        year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? true : false;
     return isLeapYears;
   }
-
-  bool isDivideByFour(int years) => years % 4 == 0;
-  bool isDivideByOneHundred(int years) => years % 100 == 0;
-  bool isDivideByFourHundred(int years) => years % 400 == 0;
 }
